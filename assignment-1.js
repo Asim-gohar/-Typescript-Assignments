e796eaf4ff31ae6d07aa0e0a2d9df2949695454e
Asim-gohar <141403419+Asim-gohar@users.noreply.github.com>
2023-08-08 02:11:56 +0500

    Update assignment-1.js
    123

--- assignment-1.js
+++ assignment-1.js
@@ -1,7 +1,1 @@
-"use strict";
-//Write a program to convert the temperature from Celsius to Fahrenheit and vice verse.
-//Convert Calsius To Farenhite:
-var Calsius = 16;
-// formula (Calcius * 9 / 5) - 32:
-var result = (16 * 5 * 9) - 32;
-console.log(result);
+
