0fbcdaf16064231147290e1c052c6ef6d072710f
Asim-gohar <141403419+Asim-gohar@users.noreply.github.com>
2023-08-18 18:41:03 +0500

    Update assignment02.ts

--- assignment02.ts
+++ assignment02.ts
@@ -143,6 +143,7 @@ let result : number = calculateSum(arrayNum) ;
 console.log(result);
 */
 /*
+                          //Problem 08
 //- Implement a program that takes a list of temperatures in Celsius as input from the user. Convert each temperature to Fahrenheit using the formula F = (C * 9/5) + 32 and store the converted temperatures in an array. Use a while loop to perform the conversion for each temperature.
 
 function convertCalciusToFarenhite(calsius : number) : number{
